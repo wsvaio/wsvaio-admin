0602befa5ad089981255ea2366fb964f65d2ab4a
wsvaio <wsvaio@qq.com>
2023-10-06 21:15:40 +0800

    wip: 待完善

--- src/layouts/administrator/views/content/components/tab/index.vue
+++ src/layouts/administrator/views/content/components/tab/index.vue
@@ -11,125 +11,125 @@ const { tabs } = tab;
 const router = useRouter();
 
 const handleUpdate = (route: RouteLocationNormalizedLoaded | RouteRecordRaw) => {
-  deepFind(auth.routes, item => item.name == route.name) && tab.appendOrReplace(route);
+	deepFind(auth.routes, item => item.name == route.name) && tab.appendOrReplace(route);
 };
 onBeforeRouteUpdate(to => handleUpdate(to));
 handleUpdate(route);
 
 const tabsRef = $ref<HTMLDivElement>();
 const tabsScrollTo = (num: number) =>
-  tabsRef?.scrollTo({
-    behavior: "smooth",
-    left: tabsRef?.scrollLeft + num,
-  });
+	tabsRef?.scrollTo({
+		behavior: "smooth",
+		left: tabsRef?.scrollLeft + num,
+	});
 const tabsScrollIntoView = (index: number) =>
-  tabsRef?.children[index]?.scrollIntoView({
-    behavior: "smooth",
+	tabsRef?.children[index]?.scrollIntoView({
+		behavior: "smooth",
 
-  });
+	});
 
 let currentDragName: RouteRecordName | null | undefined = "";
 const dragenter = (item: RouteLocationNormalizedLoaded | RouteRecordRaw) => {
-  if (!currentDragName || currentDragName == item.name) return;
-  const index = tab.findIndexByName(item.name);
-  const t = tab.findByName(currentDragName);
-  if (!t) return;
-  tab.deleteByName(currentDragName);
-  tab.insert(index, t);
-  currentDragName = "";
-  setTimeout(() => (currentDragName = t.name), 333);
+	if (!currentDragName || currentDragName == item.name) return;
+	const index = tab.findIndexByName(item.name);
+	const t = tab.findByName(currentDragName);
+	if (!t) return;
+	tab.deleteByName(currentDragName);
+	tab.insert(index, t);
+	currentDragName = "";
+	setTimeout(() => (currentDragName = t.name), 333);
 };
 const close = (item: RouteLocationNormalizedLoaded | RouteRecordRaw) => {
-  tabs.splice(tab.findIndexByName(item.name), 1);
-  if (route.name == item.name) router.push([...tabs].reverse()[0]);
+	tabs.splice(tab.findIndexByName(item.name), 1);
+	if (route.name == item.name) router.push([...tabs].reverse()[0]);
 };
 const log = console.log;
 </script>
 
 <template>
-  <nav class="administrator-tab" :style="{ height: `${setting.tabHeight}px` }">
-    <n-button text @click="tabsScrollTo(-100)">
-      <template #icon>
-        <n-icon>
-          <i-ion:ios-arrow-right class="rotate-[180deg]" />
-        </n-icon>
-      </template>
-    </n-button>
-    <section
-      ref="tabsRef"
-      class="tabs"
-      @mousewheel="tabsScrollTo($event.deltaY)"
-      @dragenter.prevent=""
-      @dragover.prevent=""
-    >
-      <transition-group name="tab">
-        <n-tag
-          v-for="(item, index) in tabs"
-          :key="item.name!"
-          draggable="true"
-          :closable="tabs.length > 1"
-          :bordered="false"
-          :type="route.name == item.name ? 'primary' : 'default'"
-          :style="{ '--n-color': 'var(--body-color)' }"
-          @dragstart="currentDragName = item.name"
-          @dragenter.prevent="dragenter(item)"
-          @dragover.prevent="dragenter(item)"
-          @drop="dragenter(item)"
-          @dragend="currentDragName = ''"
-          @click="tabsScrollIntoView(index), router.push(item)"
-          @close="close(item)"
-        >
-          {{ item.meta?.title }}
-        </n-tag>
-      </transition-group>
-    </section>
+	<nav class="administrator-tab" :style="{ height: `${setting.tabHeight}px` }">
+		<n-button text @click="tabsScrollTo(-100)">
+			<template #icon>
+				<n-icon>
+					<i-ion:ios-arrow-right class="rotate-[180deg]" />
+				</n-icon>
+			</template>
+		</n-button>
+		<section
+			ref="tabsRef"
+			class="tabs"
+			@mousewheel="tabsScrollTo($event.deltaY)"
+			@dragenter.prevent=""
+			@dragover.prevent=""
+		>
+			<transition-group name="tab">
+				<n-tag
+					v-for="(item, index) in tabs"
+					:key="item.name!"
+					draggable="true"
+					:closable="tabs.length > 1"
+					:bordered="false"
+					:type="route.name == item.name ? 'primary' : 'default'"
+					:style="{ '--n-color': 'var(--body-color)' }"
+					@dragstart="currentDragName = item.name"
+					@dragenter.prevent="dragenter(item)"
+					@dragover.prevent="dragenter(item)"
+					@drop="dragenter(item)"
+					@dragend="currentDragName = ''"
+					@click="tabsScrollIntoView(index), router.push(item)"
+					@close="close(item)"
+				>
+					{{ item.meta?.title }}
+				</n-tag>
+			</transition-group>
+		</section>
 
-    <n-button text @click="tabsScrollTo(100)">
-      <template #icon>
-        <n-icon>
-          <i-ion:ios-arrow-right />
-        </n-icon>
-      </template>
-    </n-button>
+		<n-button text @click="tabsScrollTo(100)">
+			<template #icon>
+				<n-icon>
+					<i-ion:ios-arrow-right />
+				</n-icon>
+			</template>
+		</n-button>
 
-    <n-dropdown
-      :options="[
-        {
-          label: '刷新当前',
-          key: '1',
+		<n-dropdown
+			:options="[
+				{
+					label: '刷新当前',
+					key: '1',
 
-          icon: () => h(IRefresh),
-        },
-        {
-          label: '关闭当前',
-          key: '2',
-          disabled: tabs.length > 1,
-          icon: () => h(IRefresh),
-        },
-        {
-          label: '关闭其他',
-          key: '3',
-          disabled: tabs.length > 1,
-          icon: () => h(IRefresh),
-        },
-        {
-          label: '关闭全部',
-          key: '4',
-          disabled: tabs.length > 1,
-          icon: () => h(IRefresh),
-        },
-      ]"
-      @select="log"
-    >
-      <n-button text>
-        <template #icon>
-          <n-icon>
-            <i-ion:ios-arrow-right class="rotate-[90deg]" />
-          </n-icon>
-        </template>
-      </n-button>
-    </n-dropdown>
-  </nav>
+					icon: () => h(IRefresh),
+				},
+				{
+					label: '关闭当前',
+					key: '2',
+					disabled: tabs.length > 1,
+					icon: () => h(IRefresh),
+				},
+				{
+					label: '关闭其他',
+					key: '3',
+					disabled: tabs.length > 1,
+					icon: () => h(IRefresh),
+				},
+				{
+					label: '关闭全部',
+					key: '4',
+					disabled: tabs.length > 1,
+					icon: () => h(IRefresh),
+				},
+			]"
+			@select="log"
+		>
+			<n-button text>
+				<template #icon>
+					<n-icon>
+						<i-ion:ios-arrow-right class="rotate-[90deg]" />
+					</n-icon>
+				</template>
+			</n-button>
+		</n-dropdown>
+	</nav>
 </template>
 
 <style lang="less">
--- src/stores/useTabStore.ts
+++ src/stores/useTabStore.ts
@@ -2,65 +2,65 @@ import { defineStore } from "pinia";
 import type { RouteLocationNormalizedLoaded, RouteRecordName, RouteRecordRaw } from "vue-router";
 
 export default defineStore("tab", {
-  state: () => ({
-    tabs: [] as (RouteLocationNormalizedLoaded | RouteRecordRaw)[],
-    excludes: [] as (RouteRecordName | undefined | null)[],
-    includes: [] as (RouteRecordName | undefined | null)[],
-    isRouteView: true,
-  }),
-  actions: {
-    findByName(name?: RouteRecordName | null) {
-      return this.tabs.find(item => item.name == name);
-    },
-    findIndexByName(name?: RouteRecordName | null) {
-      return this.tabs.findIndex(item => item.name == name);
-    },
-    insert(index: number, route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
-      this.tabs.splice(index, 0, { ...route });
-    },
-    append(route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
-      if (!this.findByName(route.name)) this.tabs.push({ ...route });
-    },
-    replace(route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
-      const index = this.findIndexByName(route.name);
-      if (index != -1) {
-        this.insert(index, route);
-        this.deleteByName(route.name);
-      }
-    },
-    appendOrReplace(route?: RouteLocationNormalizedLoaded | RouteRecordRaw) {
-      if (!route) return;
-      const index = this.findIndexByName(route.name);
-      if (index != -1) this.replace(route);
-      else this.append(route);
-    },
-    deleteByName(name?: RouteRecordName | null) {
-      const index = this.findIndexByName(name);
-      if (index != -1) this.tabs.splice(index, 1);
-    },
+	state: () => ({
+		tabs: [] as (RouteLocationNormalizedLoaded | RouteRecordRaw)[],
+		excludes: [] as (RouteRecordName | undefined | null)[],
+		includes: [] as (RouteRecordName | undefined | null)[],
+		isRouteView: true,
+	}),
+	actions: {
+		findByName(name?: RouteRecordName | null) {
+			return this.tabs.find(item => item.name == name);
+		},
+		findIndexByName(name?: RouteRecordName | null) {
+			return this.tabs.findIndex(item => item.name == name);
+		},
+		insert(index: number, route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
+			this.tabs.splice(index, 0, { ...route });
+		},
+		append(route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
+			if (!this.findByName(route.name)) this.tabs.push({ ...route });
+		},
+		replace(route: RouteLocationNormalizedLoaded | RouteRecordRaw) {
+			const index = this.findIndexByName(route.name);
+			if (index != -1) {
+				this.insert(index, route);
+				this.deleteByName(route.name);
+			}
+		},
+		appendOrReplace(route?: RouteLocationNormalizedLoaded | RouteRecordRaw) {
+			if (!route) return;
+			const index = this.findIndexByName(route.name);
+			if (index != -1) this.replace(route);
+			else this.append(route);
+		},
+		deleteByName(name?: RouteRecordName | null) {
+			const index = this.findIndexByName(name);
+			if (index != -1) this.tabs.splice(index, 1);
+		},
 
-    async refresh(name?: RouteRecordName | null) {
-      if (!name) return;
-      this.excludes.push(name);
-      await nextTick();
-      this.isRouteView = false;
-      await nextTick();
-      this.isRouteView = true;
-      this.excludes.pop();
-    },
-  },
-  getters: {
-    include(): string[] {
-      return [
-        ...this.includes.map(item => String(item)),
-        ...this.tabs
-          .map(item => item.name)
-          .filter(item => !!item)
-          .map(item => String(item)),
-      ];
-    },
-    exclude(): string[] {
-      return this.excludes.filter(item => !!item).map(item => String(item));
-    },
-  },
+		async refresh(name?: RouteRecordName | null) {
+			if (!name) return;
+			this.excludes.push(name);
+			await nextTick();
+			this.isRouteView = false;
+			await nextTick();
+			this.isRouteView = true;
+			this.excludes.pop();
+		},
+	},
+	getters: {
+		include(): string[] {
+			return [
+				...this.includes.map(item => String(item)),
+				...this.tabs
+					.map(item => item.name)
+					.filter(item => !!item)
+					.map(item => String(item)),
+			];
+		},
+		exclude(): string[] {
+			return this.excludes.filter(item => !!item).map(item => String(item));
+		},
+	},
 });
